76103eb8b7be5790326da787995e100dc5a37cfc
Nitaa1904 <nitafitrotul1904@gmail.com>
2024-11-11 01:00:52 +0700

    first commit

---
+++ src/components/Fragments/FormRegister.jsx
@@ -0,0 +1,34 @@
+import Button from "../Elements/button/Button";
+import { InputForm } from "../Elements/Input";
+
+export const FormRegister = () => {
+  return (
+    <form>
+      <InputForm
+        label={"FullName"}
+        type={"text"}
+        placeholder={"Insert Your Name here"}
+        name={"fullName"}
+      />
+      <InputForm
+        label={"Email"}
+        type={"email"}
+        placeholder={"example@gmail.com"}
+        name={"email"}
+      />
+      <InputForm
+        label={"Password"}
+        type={"password"}
+        placeholder={"*****"}
+        name={"password"}
+      />
+      <InputForm
+        label={"Confirm Password"}
+        type={"password"}
+        placeholder={"*****"}
+        name={"password"}
+      />
+      <Button>Register</Button>
+    </form>
+  );
+};
